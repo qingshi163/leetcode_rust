1af5bafd75d24f7852341f1d39e7f69070373f28
Kangzhi Shi <shikangzhi@gmail.com>
2019-12-15 13:33:36 +0000

    update lc4

--- src/lc4.rs
+++ src/lc4.rs
@@ -1,7 +1,15 @@
 
+fn find_median_single(nums: Vec<i32>, head: usize, tail: usize) -> f64 {
+
+}
+
 #[allow(dead_code)]
-#[allow(non_snake_case)]
 fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+    if nums.is_empty()
+    let head1 = 0;
+    let head2 = 0;
+    let tail1 = nums1.len() - 1;
+    let tail2 = nums2.len() - 1;
 }
 
 #[cfg(test)]
