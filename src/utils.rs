3c5bc00bd03df04b30964db5511a8e2dc2cd968d
Kangzhi Shi <shikangzhi@gmail.com>
2020-01-03 14:37:56 +0000

    lc234 add test, utils add ListNode and to_linked_list

--- src/utils.rs
+++ src/utils.rs
@@ -16,3 +16,32 @@ macro_rules! vec_sort {
         tmp
     }};
 }
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  #[allow(dead_code)]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+
+#[allow(dead_code)]
+pub fn to_linked_list(v: &[i32]) -> Option<Box<ListNode>> {
+    let mut curr = None;
+    for &val in v.iter().rev() {
+        let mut node = ListNode::new(val);
+        node.next = curr;
+        curr = Some(Box::new(node));
+    }
+    curr
+}
