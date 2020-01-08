7eeb9c7b55afb6013f4ec574e4f3b16ce36ff075
Kangzhi Shi <shikangzhi@gmail.com>
2020-01-08 16:22:49 +0000

    lc100 passed

---
+++ src/lc100.rs
@@ -0,0 +1,22 @@
+
+
+use std::rc::Rc;
+use std::cell::RefCell;
+use super::utils::TreeNode;
+use super::utils::to_tree;
+#[allow(dead_code)]
+fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+    p == q
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_100() {
+        assert!(is_same_tree(None, None));
+        assert!(is_same_tree(tree![1,2,3], tree![1,2,3]));
+        assert!(is_same_tree(tree![1,2,3,null,4], tree![1,2,3,null,4]));
+    }
+}
