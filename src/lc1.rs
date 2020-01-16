0701857645922022857266d379273dd00e538ffa
Kangzhi Shi <shikangzhi@gmail.com>
2020-01-16 11:10:39 +0000

    lc1 update

--- src/lc1.rs
+++ src/lc1.rs
@@ -1,15 +1,15 @@
+use std::collections::HashMap;
 
 #[allow(dead_code)]
 fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let len = nums.len();
-    for i in 0..(len-1) {
-        for j in (i+1)..len {
-            if nums[i] + nums[j] == target {
-                return vec![i as i32, j as i32];
-            }
+    let mut map = HashMap::new();
+    for (i, num) in nums.iter().enumerate() {
+        match map.get(&(target - num)) {
+            Some(val) => return vec![*val, i as i32],
+            None => { map.insert(num, i as i32); }
         }
     }
-    return Vec::new();
+    Vec::new()
 }
 
 #[cfg(test)]
