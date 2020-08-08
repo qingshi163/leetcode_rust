15aadea8b2378762d7d45ca53aea28fff6a69cbb
Kangzhi Shi <shikangzhi@gmail.com>
2020-08-08 10:28:29 +0200

    design_hashset pass

--- src/design_hashset.rs
+++ src/design_hashset.rs
@@ -1,7 +1,5 @@
-use std::rc::Rc;
-use std::cell::RefCell;
-use super::utils::*;
 
+#[derive(Clone)]
 enum Node {
     Nil,
     One(i32),
@@ -18,6 +16,7 @@ struct MyHashSet {
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
+#[allow(dead_code)]
 impl MyHashSet {
 
     /** Initialize your data structure here. */
@@ -33,18 +32,73 @@ impl MyHashSet {
             bulk: vec![Node::Nil; capacity]
         }
     }
-    
-    fn add(&self, key: i32) {
-        
+
+    fn rebuild(&mut self, capacity: usize) {
+        let mut new_self = MyHashSet::with_capacity(capacity);
+        for node in &self.bulk {
+            match node {
+                Node::Nil => {},
+                Node::One(val) => new_self.add(*val),
+                Node::More(v) => v.iter().for_each(|&x| new_self.add(x))
+            }
+        }
+        self.bulk = new_self.bulk
     }
-    
-    fn remove(&self, key: i32) {
-        
+
+    fn add(&mut self, key: i32) {
+        if self.contains(key) { return; }
+        if self.length * 2 >= self.bulk.len() {
+            self.rebuild(std::cmp::max(self.bulk.len() * 2, 8));
+        }
+        let index = self.hash(key);
+        match &mut self.bulk[index] {
+            Node::Nil => self.bulk[index] = Node::One(key),
+            Node::One(val) => self.bulk[index] = Node::More(vec![*val, key]),
+            Node::More(v) => v.push(key)
+        }
+        self.length += 1;
+    }
+
+    fn remove(&mut self, key: i32) {
+        if self.length == 0 { return; }
+        let index = self.hash(key);
+        match &mut self.bulk[index] {
+            Node::Nil => {},
+            Node::One(val) => {
+                if *val == key {
+                    self.bulk[index] = Node::Nil;
+                    self.length -= 1;
+                }
+            },
+            Node::More(v) => {
+                for i in 0..v.len() {
+                    if v[i] == key {
+                        v.swap_remove(i);
+                        self.length -= 1;
+                        break;
+                    }
+                }
+            }
+        }
     }
     
     /** Returns true if this set contains the specified element */
     fn contains(&self, key: i32) -> bool {
+        if self.length == 0 { return false; }
+        let index = self.hash(key);
+        match &self.bulk[index] {
+            Node::Nil => false,
+            Node::One(val) => *val == key,
+            Node::More(v) => v.iter().any(|&x| x == key)
+        }
+    }
 
+    fn hash(&self, key: i32) -> usize {
+        use std::collections::hash_map::DefaultHasher;
+        use std::hash::{ Hash, Hasher };
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        (hasher.finish() % self.bulk.len() as u64) as usize
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -63,6 +63,7 @@ mod lc258;
 mod lc977;
 mod invert_binary_tree;
 mod binary_tree_zigzag;
+mod design_hashset;
 
 fn main() {
     println!("Use cargo test to run the code");
